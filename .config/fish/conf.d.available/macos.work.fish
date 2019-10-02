91900b24fc282cdb353954bc0ca93bf1ca9cd2b5
François Ubald Brien <fbrien@zendesk.com>
2019-10-02 16:19:00 -0400

    Update to mac configs for work

--- .config/fish/conf.d.available/macos.work.fish
+++ .config/fish/conf.d.available/macos.work.fish
@@ -1,2 +1,9 @@
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc
+# DOESN'T SEEM TO EXIST source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+alias k="kubectl"
+alias kcontexts="kubectl config get-contexts"
+alias kuse="kubectl config use-context"
+alias kpods="kubectl get pods"
+alias kscale="kubectl scale deploy"
+alias kwpods="watch \"kubectl get pods\""
