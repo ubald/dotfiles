7d047be98493ccbbe9205284030ee7acad486554
François Ubald Brien <fbrien@zendesk.com>
2020-02-05 12:40:04 -0500

    Work adjustments

--- .config/fish/conf.d.available/macos.work.fish
+++ .config/fish/conf.d.available/macos.work.fish
@@ -1,13 +1,6 @@
 # DOESN'T SEEM TO EXIST source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 
-alias k="kubectl"
-alias kcontexts="kubectl config get-contexts"
-alias kuse="kubectl config use-context"
-alias kpods="kubectl get pods"
-alias kscale="kubectl scale deploy"
-alias kwpods="watch \"kubectl get pods\""
-
 alias smooch_clean="rm -rf ./dist; mkdir ./dist; ln -s $PWD/resources $PWD/dist/resources; ln -s $PWD/views $PWD/dist/views"
 alias smooch_update="smooch_clean; npm install"
 alias smooch_reset="rm -rf ./node_modules; smooch_update"
---
+++ .config/fish/conf.d/unix.fish
@@ -0,0 +1,1 @@
+/Users/fbrien/.config/fish/conf.d.available/unix.fish
