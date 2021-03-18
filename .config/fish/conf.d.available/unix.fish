d7e33b8a1d6454a8b8090bc3897aa285df9a76ea
François Ubald Brien <fbrien@zendesk.com>
2021-03-18 10:46:50 -0400

    Last changes before returning work laptop

--- .config/fish/conf.d.available/unix.fish
+++ .config/fish/conf.d.available/unix.fish
@@ -1,3 +1,6 @@
 # Unix configuration
 
 set -gx fish_user_paths $HOME/.local/bin $fish_user_paths
+
+alias docker-clean-containers="docker rm (docker ps -a -q)"
+alias docker-remove-untagged-images='docker rmi (docker images | grep "^<none>" | awk "{print $3}")'
