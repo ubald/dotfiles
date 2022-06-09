489c464c35a356115e7384db4b3aaa5272e83138
François Ubald Brien <ubald@ubald.dev>
2022-06-09 14:29:40 -0400

    Some changes to OBS setup

--- setup_arch_system.sh
+++ setup_arch_system.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
+# From Installation Guide:
+
+# Set keyboard layout (default is US)
+# ls /usr/share/kbd/keymaps/**/*.map.gz
+# loadkeys us
+
+# Check that this doesn't give any erros
+# ls /sys/firmware/efi/efivars
+
+# Connect to the internet
+# ip link
+# ping archlinux.org
+
+# Update system clock
+# timedatectl set-ntp true
+# timedatectl status
+
+# Partition the disk
+# fdisk -l
+
 # Enable Network
 systemctl enable --now dhcpcd.service
 
